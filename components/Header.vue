4c47014e53921a02bb70454f665b8a1774ae196f
batihandev <batihandev@gmail.com>
2023-10-10 21:25:58 +0300

    lame ass word changed

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts"></script>
 <template>
   <div
-    class="bg-background text-textmain font-goldman flex h-[60px] w-full items-center justify-center gap-4 py-4 text-lg font-bold"
+    class="flex h-[60px] w-full items-center justify-center gap-4 bg-background py-4 font-goldman text-lg font-bold text-textmain"
   >
     <nuxt-link to="/">Test</nuxt-link>
     <nuxt-link to="/original">Original</nuxt-link>
     <nuxt-link to="/middle">Middle</nuxt-link>
-    <nuxt-link to="/pixi-test">Gifted</nuxt-link>
+    <nuxt-link to="/pixi-test">Gif</nuxt-link>
   </div>
 </template>
