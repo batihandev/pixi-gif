2753c5df87d8f92242e504d35325104eaefb23df
batihandev <batihandev@gmail.com>
2023-10-10 19:56:43 +0300

    init

---
+++ components/ColorfullBg.vue
@@ -0,0 +1,219 @@
+<script setup lang="ts">
+import * as PIXI from "pixi.js";
+import { createNoise2D } from "simplex-noise";
+import debounce from "lodash.debounce";
+import hsl from "hsl-to-hex";
+import { KawaseBlurFilter } from "@pixi/filter-kawase-blur";
+const noise2D = createNoise2D();
+const orbPool = [];
+const maxOrbs = 10; // Adjust this number based on your needs
+
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+function map(n, start1, end1, start2, end2) {
+  return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
+}
+// ColorPalette class
+class ColorPalette {
+  constructor() {
+    // Predefined hue values for the specified colors
+    this.hueChoices = [215, 230, 280, 320];
+    this.currentHueIndex = 0; // Initialize the index
+    this.setCustomProperties();
+  }
+
+  randomColor() {
+    // Get the current hue from the choices
+    const currentHue = this.hueChoices[this.currentHueIndex];
+    // Increment the index for the next call
+    this.currentHueIndex = (this.currentHueIndex + 1) % this.hueChoices.length;
+
+    // Convert the hue to an HSL color
+    const color = hsl(currentHue, 95, 50);
+    return color.replace("#", "0x");
+  }
+
+  setCustomProperties() {
+    // No need to set custom properties since you have predefined hues
+  }
+}
+// Orb class
+class Orb {
+  // Pixi takes hex colors as hexidecimal literals (0x rather than a string with '#')
+  constructor(fill = 0x000000) {
+    // bounds = the area an orb is "allowed" to move within
+    this.bounds = this.setBounds();
+    // initialise the orb's { x, y } values to a random point within it's bounds
+    this.x = random(this.bounds["x"].min, this.bounds["x"].max);
+    this.y = random(this.bounds["y"].min, this.bounds["y"].max);
+
+    // how large the orb is vs it's original radius (this will modulate over time)
+    this.scale = 1;
+
+    // what color is the orb?
+    this.fill = fill;
+
+    // the original radius of the orb, set relative to window height
+    this.radius = random(window.innerHeight / 6, window.innerHeight / 3);
+
+    // starting points in "time" for the noise/self similar random values
+    this.xOff = random(0, 1000);
+    this.yOff = random(0, 1000);
+    // how quickly the noise/self similar random values step through time
+    this.inc = 0.002;
+
+    // PIXI.Graphics is used to draw 2d primitives (in this case a circle) to the canvas
+    this.graphics = new PIXI.Graphics();
+    this.graphics.alpha = 0.825;
+
+    // 250ms after the last window resize event, recalculate orb positions.
+    window.addEventListener(
+      "resize",
+      debounce(() => {
+        this.bounds = this.setBounds();
+      }, 250)
+    );
+  }
+
+  setBounds() {
+    // Calculate the center of the screen
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+
+    // Set the maximum distance from the center
+    const maxDist = Math.min(centerX, centerY) - 150; // Subtract 50 pixels
+
+    // Allow each orb to move x distance away from its center
+    return {
+      x: {
+        min: centerX - maxDist,
+        max: centerX + maxDist,
+      },
+      y: {
+        min: centerY - maxDist,
+        max: centerY + maxDist,
+      },
+    };
+  }
+
+  update() {
+    // Generate noise values once per frame
+    const xNoise = noise2D(this.xOff, this.xOff);
+    const yNoise = noise2D(this.yOff, this.yOff);
+    const scaleNoise = noise2D(this.xOff, this.yOff);
+
+    // Map the noise values to the orb's properties
+    const mappedX = map(
+      xNoise,
+      -1,
+      1,
+      this.bounds["x"].min,
+      this.bounds["x"].max
+    );
+    const mappedY = map(
+      yNoise,
+      -1,
+      1,
+      this.bounds["y"].min,
+      this.bounds["y"].max
+    );
+    const mappedScale = map(scaleNoise, -1, 1, 0.5, 1);
+
+    // Update orb properties
+    this.x = mappedX;
+    this.y = mappedY;
+    this.scale = mappedScale;
+
+    // Step through "time"
+    this.xOff += this.inc;
+    this.yOff += this.inc;
+  }
+  reset(fill) {
+    this.fill = fill;
+    this.radius = random(window.innerHeight / 6, window.innerHeight / 3);
+    this.xOff = random(0, 1000);
+    this.yOff = random(0, 1000);
+    this.scale = 1;
+    this.x = random(this.bounds["x"].min, this.bounds["x"].max);
+    this.y = random(this.bounds["y"].min, this.bounds["y"].max);
+  }
+
+  isOutOfBounds() {
+    return (
+      this.x < this.bounds["x"].min ||
+      this.x > this.bounds["x"].max ||
+      this.y < this.bounds["y"].min ||
+      this.y > this.bounds["y"].max
+    );
+  }
+  render() {
+    // update the PIXI.Graphics position and scale values
+    this.graphics.x = this.x;
+    this.graphics.y = this.y;
+    this.graphics.scale.set(this.scale);
+
+    // clear anything currently drawn to graphics
+    this.graphics.clear();
+
+    // tell graphics to fill any shapes drawn after this with the orb's fill color
+    this.graphics.beginFill(this.fill);
+    // draw a circle at { 0, 0 } with it's size set by this.radius
+    this.graphics.drawCircle(0, 0, this.radius);
+    // let graphics know we won't be filling in any more shapes
+    this.graphics.endFill();
+  }
+}
+const canvas = ref(null);
+onMounted(() => {
+  const app = new PIXI.Application({
+    // render to <canvas class="orb-canvas"></canvas>
+    view: canvas.value,
+    // auto adjust size to fit the current window
+    resizeTo: window,
+    // transparent background, we will be creating a gradient background later using CSS
+    transparent: true,
+    backgroundAlpha: 0,
+  });
+  app.stage.filters = [new KawaseBlurFilter(30, 10, true)];
+  const colorPalette = new ColorPalette();
+
+  for (let i = 0; i < maxOrbs; i++) {
+    const orb = new Orb(colorPalette.randomColor());
+    app.stage.addChild(orb.graphics);
+    orbPool.push(orb);
+  }
+  if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    app.ticker.maxFPS = 60; // Set the maxFPS to 10
+    app.ticker.add(() => {
+      orbPool.forEach((orb) => {
+        // Check if orb is outside the bounds
+        if (orb.isOutOfBounds()) {
+          // Remove the orb from the stage
+          app.stage.removeChild(orb.graphics);
+
+          // Reset the orb and push it back into the pool
+          orb.reset(colorPalette.randomColor());
+          app.stage.addChild(orb.graphics);
+        }
+        orb.update();
+        orb.render();
+      });
+    });
+  } else {
+    orbs.forEach((orb) => {
+      orb.update();
+      orb.render();
+    });
+  }
+});
+</script>
+<template>
+  <div>
+    <canvas
+      ref="canvas"
+      class="orb-canvas w-[100dvw] !bg-transparent"
+      style="background-color: transparent"
+    ></canvas>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,113 @@
+import * as fs from "fs";
+const isSecure = fs.existsSync("./ssl.key");
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@nuxt/image-edge",
+    "@nuxtjs/google-fonts",
+  ],
+  tailwindcss: {
+    // Options
+  },
+  typescript: {
+    shim: false,
+    strict: true,
+  },
+  googleFonts: {
+    preconnect: true,
+    download: true,
+    inject: true,
+    display: "swap",
+    families: {
+      Teko: {
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      },
+      Goldman: {
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      },
+      Kanit: {
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      },
+    },
+  },
+  app: {
+    head: {
+      titleTemplate: "Test",
+      meta: [
+        {
+          name: "msapplication-TileColor",
+          content: "#00A3E0",
+        },
+        {
+          name: "theme-color",
+          content: "#ffffff",
+        },
+        {
+          name: "description",
+          content: "Empowering AI Technologies, Join The Movement.",
+        },
+        {
+          property: "og:url",
+          content: "#",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:title",
+          content: "Test",
+        },
+        {
+          property: "og:description",
+          content: "Empowering AI Technologies, Join The Movement.",
+        },
+      ],
+      // link: [
+      //   {
+      //     rel: "apple-touch-icon",
+      //     sizes: "180x180",
+      //     href: "/apple-touch-icon.png",
+      //   },
+      //   {
+      //     rel: "icon",
+      //     type: "image/png",
+      //     sizes: "16x16",
+      //     href: "/favicon-16x16.png",
+      //   },
+      //   {
+      //     rel: "icon",
+      //     type: "image/png",
+      //     sizes: "32x32",
+      //     href: "/favicon-32x32.png",
+      //   },
+      //   {
+      //     rel: "manifest",
+      //     href: "/site.webmanifest",
+      //   },
+      // ],
+    },
+  },
+  css: ["mosha-vue-toastify/dist/style.css"],
+
+  vite: {
+    server: isSecure
+      ? {
+          https: {
+            key: fs.readFileSync("./ssl.key"),
+            cert: fs.readFileSync("./ssl.crt"),
+          },
+          hmr: {
+            protocol: "wss",
+          },
+        }
+      : {},
+    logLevel: "info",
+    optimizeDeps: {
+      include: ["@headlessui/vue", "vue", "pinia"],
+    },
+  },
+});
+
